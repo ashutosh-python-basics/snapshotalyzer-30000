696c193d5f55be029b9d5705837ff7cee19b385f
Admin <lntinfotech@INFEDLNT10123.Ednodc.lntinfotech.com>
2019-05-08 07:28:00 -0400

    code added to skipp snapshot creation if another one is inprogress

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -21,6 +21,10 @@ def filter_instances(project):
 
     return instances
 
+def has_pending_snapshot(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+
 @click.group()
 def cli():
     """Shotty manages snapshots"""
@@ -97,6 +101,10 @@ def create_snapshots(project):
         i.wait_until_stopped()
 
         for v in i.volumes.all():
+            if has_pending_snapshot(v):
+                print("Skipping {0}, snapshot already in progress",format(v.id))
+                continue
+                
             print("creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="CReated by snapshotalyzer 30000")
 
